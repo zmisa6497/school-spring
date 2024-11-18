e7f8a7d2fbc534f311d7235b86cc86d433a98332
zmisa <zmisa6497@gmail.com>
2024-11-18 11:33:52 +0100

    Add Postman collection for API testing

--- src/main/java/dev/misha/school/controller/StudentController.java
+++ src/main/java/dev/misha/school/controller/StudentController.java
@@ -1,15 +1,12 @@
 package dev.misha.school.controller;
 
 
-import dev.misha.school.exception.GradeSaveException;
-import dev.misha.school.exception.JournalException;
 import dev.misha.school.model.Student;
 import dev.misha.school.service.StudentService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.jetbrains.annotations.NotNull;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,40 +18,40 @@ import java.util.List;
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class StudentController {
 
-
-    @Autowired
     StudentService studentService;
 
-
     @PostMapping("/add")
-    public void addStudent(@RequestParam String name) {
+    public ResponseEntity<String> addStudent(@RequestParam String name) {
         studentService.addStudent(name);
+        return ResponseEntity.ok("Student added successfully");
     }
 
     @DeleteMapping("/remove")
-    public void removeStudent(@RequestParam String name) {
+    public ResponseEntity<String> removeStudent(@RequestParam String name) {
         studentService.removeStudent(name);
+        return ResponseEntity.ok("Student removed successfully");
     }
 
     @PostMapping("/{name}/grades")
-    public void addGrades(@PathVariable String name, @RequestParam String gradesInput) {
-        studentService.addGrades(name,gradesInput);
+    public ResponseEntity<String> addGrades(@PathVariable String name, @RequestParam String gradesInput) {
+        studentService.addGrades(name, gradesInput);
+        return ResponseEntity.ok("Grades added successfully");
     }
 
     @GetMapping("/{name}")
-    public void getStudentByName(@PathVariable String name) {
-        studentService.getStudentByName(name);
+    public ResponseEntity<Student> getStudentByName(@PathVariable String name) {
+        Student student = studentService.getStudentByName(name);
+        return ResponseEntity.ok(student);
     }
 
-    @NotNull
     @GetMapping("/all")
-    public List<Student> getAll() {
-        return studentService.getAll();
+    public ResponseEntity<List<Student>> getAll() {
+        return ResponseEntity.ok(studentService.getAll());
     }
 
     @GetMapping("/{name}/average")
-    public void calculateAverageGrade(@PathVariable String name) {
-        studentService.calculateAverageGrade(name);
+    public ResponseEntity<Double> calculateAverageGrade(@PathVariable String name) {
+        double average = studentService.calculateAverageGrade(name);
+        return ResponseEntity.ok(average);
     }
-
 }
